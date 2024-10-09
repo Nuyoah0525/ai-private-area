cff0760c04b641648ef4c65f0cb46e0a9a10ff68
zhangyh <1056406288@qq.com>
2024-10-09 14:35:09 +0800

    init

---
+++ src/main/java/com/zhangyh/aiprivatearea/AiPrivateAreaApplication.java
@@ -0,0 +1,13 @@
+package com.zhangyh.aiprivatearea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPrivateAreaApplication {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(AiPrivateAreaApplication.class, args);
+  }
+  
+}
---
+++ src/test/java/com/zhangyh/aiprivatearea/AiPrivateAreaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhangyh.aiprivatearea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiPrivateAreaApplicationTests {
+  
+  @Test
+  void contextLoads() {
+  }
+  
+}
